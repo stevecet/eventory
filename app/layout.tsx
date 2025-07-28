b133c47bcb405248fb83354031a2399073a70eac
stevecet <steveceto@outlook.fr>
2025-07-28 17:08:15 +0100

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Eventory',
+  description: 'Created by Steveceto',
+  generator: 'Steveceto',
 }
 
 export default function RootLayout({
